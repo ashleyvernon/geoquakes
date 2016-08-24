40c0fad6ad1406f49a9dc640b461d0daeba53b77
Scott Whitman <scott.w.whitman@gmail.com>
2016-08-24 16:11:29 -0700

    our map is running with earthquake data

--- scripts/app.js
+++ scripts/app.js
@@ -6,5 +6,49 @@ var template;
 
 $(document).on("ready", function() {
 
+  $.ajax({
+      method: 'GET',
+      url: weekly_quakes_endpoint,
+      dataType: 'json',
+      success: onSuccess
+  });
+
+  function onSuccess(json) {
+    console.log(json);
+
+    var source = $('#earthquake-template').html();
+    var earthquakeTemplate = Handlebars.compile(source);
+    for(var i=0; i<json.features.length; i++){
+      var earthquakeHtml = earthquakeTemplate({
+        earthquakeTitle: json.features[i].properties.title
+      });
+      $('#info').append(earthquakeHtml);
+    };
+
+    initMap();
+
+    function initMap() {
+        var mapDiv = document.getElementById('map');
+        var myLatLng = {lat: 37.78, lng: -122.44};
+        var map = new google.maps.Map(mapDiv, {
+            center: myLatLng,
+            zoom: 8
+        });
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
+        for(var i=0; i<json.features.length; i++){
+          var marker = new google.maps.Marker({
+            position: {lat: json.features[i].geometry.coordinates[1], lng: json.features[i].geometry.coordinates[0]},
+            map: map,
+            title: 'Hello World!'
+          });
+        }
+    }
+
+  };
+
 
 });
